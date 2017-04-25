468fc01de39f083c8a73be4e0e3fe66aea03b182
40423109 <40423109@gm.nfu.edu.tw>
2017-04-25 16:45:03 +0800

    01

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'KMOL'
-SITENAME = '2016Fall 課程網誌 (虎尾科大MDE)'
+SITENAME = '2017Springcd 協同產品設計實習 (MDE)'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
